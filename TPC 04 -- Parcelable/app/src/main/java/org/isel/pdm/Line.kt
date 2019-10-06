e032a852178ee6849d2019401299367259a2c6a6
tiagofsmartins <tiagofsmartins@hotmail.com>
2019-10-06 20:58:35 +0100

    tpc4 ponto1

--- TPC 04 -- Parcelable/app/src/main/java/org/isel/pdm/Line.kt
+++ TPC 04 -- Parcelable/app/src/main/java/org/isel/pdm/Line.kt
@@ -9,13 +9,34 @@ import java.io.Serializable
 
 class Line (val x: Float, val y: Float) :  Serializable , Parcelable {
 
+    constructor(parcel: Parcel): this(
+        parcel.readFloat(),
+        parcel.readFloat()
+    ){
+        add(x, y)
+        //parcel.readTypedList(points, XyPair.CREATOR)
+    }
+
     private val points : MutableList<XyPair> = mutableListOf()
 
-    /*
-    constructor(x:Float, y:Float){
-        add(x,y)
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeFloat(x)
+        dest?.writeFloat(y)
+        //dest?.writeTypedList(points)
     }
-     */
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<Line> {
+        override fun createFromParcel(parcel: Parcel): Line {
+            return Line(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Line?> {
+            return arrayOfNulls(size)
+        }
+    }
 
     fun add(x: Float, y: Float) {
         points.add(XyPair(x, y))
@@ -25,39 +46,11 @@ class Line (val x: Float, val y: Float) :  Serializable , Parcelable {
         val paint = Paint()
         paint.setColor(Color.BLUE)
         paint.strokeWidth = 20f
-
-        points.reduce { prev, curr ->
-            canvas.drawLine(prev.x, prev.y, curr.x, curr.y, paint)
-            curr
-        }
-    }
-
-
-
-    constructor(parcel: Parcel): this(
-        parcel.readFloat(),
-        parcel.readFloat()
-    ){
-        add(x, y)
-        //parcel.readTypedList(points, XyPair.CREATOR)
-    }
-
-    override fun writeToParcel(dest: Parcel?, flags: Int) {
-        dest?.writeFloat(x)
-        dest?.writeFloat(y)
-        dest?.writeTypedList(points)
-    }
-
-    override fun describeContents(): Int = 0
-
-   companion object CREATOR : Parcelable.Creator<Line> {
-       override fun createFromParcel(parcel: Parcel): Line {
-           return Line(parcel)
-       }
-
-       override fun newArray(size: Int): Array<Line?> {
-           return arrayOfNulls(size)
-       }
+        if(points.size>0)
+            points.reduce { prev, curr ->
+                canvas.drawLine(prev.x, prev.y, curr.x, curr.y, paint)
+                curr
+            }
     }
 
 
